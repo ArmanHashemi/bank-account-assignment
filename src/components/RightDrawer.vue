cdd6127d2225b46fb74b3294db1f9f943bc71b98
Arman Hashemi <arman.power.3@gmail.com>
2023-08-06 01:09:40 +0330

    feat (drawer menu): add drawer menu and toolbar

---
+++ src/components/RightDrawer.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="flex flex-col w-full h-full">
+    <div class="w-full flex p-2 space-y-4">
+      <div class="w-full flex items-center justify-end space-x-2">
+        <img src="~assets/images/logo.svg" class="h-8">
+      </div>
+      <div class="bg-gray-100 w-full flex items-center justify-between p-2 rounded-xl">
+        <img src="~assets/icons/item5.svg" class="h-8 w-8" />
+        <div class="flex items-center space-x-8">
+          <span class="text-md text-bold">شرکت اپل</span>
+          <q-icon name="apple" size="md" />
+        </div>
+
+      </div>
+    </div>
+    <div class="flex-1 overflow-scroll">
+      <q-expansion-item
+        switch-toggle-side
+        expand-separator
+        no-border
+        :default-opened="item.link === 'treasury'"
+        :expand-icon-class="{'opacity-0' : !item.subs.length}"
+        v-for="(item,i) in menu" :key="i"
+      >
+        <template v-slot:header>
+          <div class="flex w-full justify-between items-center">
+            <span class="">{{ item.label }}</span>
+            <img :src="item.icon" class="h-6 w-6" />
+          </div>
+        </template>
+        <div class="flex w-full p-2 items-center justify-start" v-if="item.subs.length">
+          <div class="w-full mx-2 p-2 border-r text-right cursor-pointer" :class="{'text-primary text-bold' : sub.link === 'create'}" v-for="(sub,j) in item.subs" :key="j">{{ sub.title }}</div>
+        </div>
+      </q-expansion-item>
+    </div>
+    <div class="flex w-full py-4 border-t">
+        <div class="w-full flex items-center justify-end space-x-8">
+          <span class="text-md text-bold">راهنما</span>
+          <img src="~assets/icons/item9.svg" class="h-8 w-8" />
+        </div>
+        <div class="w-full flex items-center justify-between">
+          <span>ورژن ۱.۰.۴</span>
+          <div class="flex items-center justify-end space-x-8">
+            <span class="text-md text-bold">پشتیبانی</span>
+            <img src="~assets/icons/item11.svg" class="h-8 w-8" />
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="js">
+defineProps({
+  menu: {
+    type: Array,
+    required: true
+  }
+});
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,24 @@
 <template>
   <q-layout view="hHr lpR ffr">
 
+    <q-header bordered class="bg-white h-20 flex items-center">
+      <q-toolbar>
+        <main-toolbar />
+        <q-btn rounded dense icon="arrow_forward" unelevated class="q-mini-drawer-hide text-grey-7 bg-white border-solid border-1 border-gray-200" style="position: absolute;right: -20px;border-width: 1px" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="rightDrawerOpen"
+      show-if-above
+      side="right"
+      bordered
+      class="px-4"
+    >
+      <right-drawer :menu="data"/>
+
+    </q-drawer>
+
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -9,5 +27,13 @@
 </template>
 
 <script setup lang="js">
+import { ref } from 'vue'
+import { data } from 'src/constants/drawer'
+import MainToolbar from "components/MainToolbar";
+import RightDrawer from "components/RightDrawer";
+const rightDrawerOpen = ref(true)
 
 </script>
+<style>
+
+</style>
